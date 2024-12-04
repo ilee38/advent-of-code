e314f7a0420163675812d21a8d20c7b0731f9cd7
Iram Lee <iram.lee@gmail.com>
2024-12-03 20:44:48 -0600

    create C# and .NET utilities library for Advent of Code

---
+++ AdventOfCode/CsharpDotNetUtils/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CsharpDotNetUtils;
+
+public static class Utils
+{
+    public static StreamReader GetStreamReaderFromTextFile(string filePath)
+    {
+        using var streamReader = new StreamReader(filePath);
+        return streamReader;
+    }
+
+    public static string ReadAllTextToString(string filePath)
+    {
+        var allText = File.ReadAllText(filePath);
+        return allText;
+    }
+
+    public static List<int> GetIntegersFromString(string line)
+    {
+        var digitsPattern = new Regex(@"\d+");
+        var digits = digitsPattern.Matches(line);
+        
+        return digits.Select(x => int.Parse(x.Value)).ToList();
+    }
+
+    public static List<double> GetDoublesFromString(string line)
+    {
+        var digitsPattern = new Regex(@"\d+");
+        var digits = digitsPattern.Matches(line);
+        
+        return digits.Select(x => double.Parse(x.Value)).ToList();
+    }
+}
