294de9d93fb32d876d9feda6b37088b006766c07
Iram Lee <iram.lee@gmail.com>
2025-05-16 19:14:15 -0600

    create AoC 2015 project

---
+++ AdventOfCode/AdventOfCode2015/Day1/Day1.cs
@@ -0,0 +1,6 @@
+namespace AdventOfCode2015.Day1;
+
+public class Day1
+{
+    
+}
---
+++ AdventOfCode/AdventOfCode2015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2015");
+        }
+    }
+}
