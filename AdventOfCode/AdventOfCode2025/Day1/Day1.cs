d5ab333e2e43891b3d590b83f3e6c8e48ad3fbbe
Iram Lee <iram.lee@gmail.com>
2025-12-02 06:14:30 -0600

    solve day1, part2

--- AdventOfCode/AdventOfCode2025/Day1/Day1.cs
+++ AdventOfCode/AdventOfCode2025/Day1/Day1.cs
@@ -37,4 +37,52 @@ public class Day1
         }
         Console.WriteLine($"Day 1, part 1: password: {zerosCount}");
     }
+
+    public static void PartTwo()
+    {
+        var zerosCount = 0;
+        var currentPosition = 50;
+        
+        using (var rotations = TextUtils.GetStreamReaderFromTextFile(@"Day1/input.txt"))
+        {
+            while (!rotations.EndOfStream)
+            {
+                var line = rotations.ReadLine();
+                var direction = line.Substring(0, 1);
+                var distance = int.Parse(line.Substring(1));
+                
+                int distanceToZero;
+                var tempDistance = distance;
+                switch (direction)
+                {
+                    case "L":   // CCW rotation
+                        while (distance > 0)
+                        {
+                            currentPosition = (currentPosition - 1) % 100;
+                            if (currentPosition == 0)
+                            {
+                                zerosCount++;
+                            }
+                            distance--;
+                        }
+                        
+                        break;
+                    case "R":   // CW rotation
+                        while (distance > 0)
+                        {
+                            currentPosition = (currentPosition + 1) % 100;
+                            if (currentPosition == 0)
+                            {
+                                zerosCount++;
+                            }
+                            distance--;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        Console.WriteLine($"Day 2, part 2: password: {zerosCount}");
+    }
 }
--- AdventOfCode/AdventOfCode2025/Program.cs
+++ AdventOfCode/AdventOfCode2025/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day1.Day1.PartOne();
+            Day1.Day1.PartTwo();
         }
     }
 }
